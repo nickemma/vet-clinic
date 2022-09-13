de2fe4781bbd3a83ed9120626010650d86470db9
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-09-13 13:53:04 +0100

    made some changes

--- queries.sql
+++ queries.sql
@@ -8,3 +8,32 @@ SELECT name, escape_attempt FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered is true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+BEGIN;
+UPDATE animal SET specie = 'unspecified';
+ROLLBACK;
+SELECT * FROM animals;
+BEGIN
+UPDATE animals SET specie = 'digimon' WHERE name LIKE '%mon';
+SELECT * FROM animals;
+UPDATE animals SET specie = 'Pokemon' WHERE specie is NULL;
+COMMIT;
+SELECT * FROM animals;
+BEGIN
+DELETE FROM animals; 
+ROLLBACK;
+SELECT * FROM animals;
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT S1;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+ROLLBACK S1;
+SELECT COUNT(*) FROM animals;
+
+SELECT COUNT(*) FROM animals WHERE escape_attempt = 0;
+
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT MAX(escape_attempt) FROM animals;
+SELECT MIN(weight_kg), MAX(weight_kg) FROM animals;
+SELECT AVG(escape_attempt) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31';
